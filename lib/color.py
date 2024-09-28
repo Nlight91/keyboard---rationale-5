ce81e295bccfced60165a982e86384fe460da9e9
Nlight91 <nicolas.david.mail@gmail.com>
2024-09-28 20:33:20 +0200

    refactor : add a preset color library

---
+++ lib/color.py
@@ -0,0 +1,26 @@
+GAMMA = bytearray(256)
+for i in range(256):
+    GAMMA[i] = int(pow(float(i)/255, 2.7) * 255 + 0.5)
+
+def rgb(string:str):
+    num = int(string, 16)
+    r = (num & 0xff0000) >> 4
+    g = (num & 0xff00) >> 2
+    b = num & 0xff
+    return GAMMA[r], GAMMA[g], GAMMA[b]
+
+RED         = rgb("ff0000")
+GREEN       = rgb("00ff00")
+BLUE        = rgb("0000ff")
+
+YELLOW      = rgb("ffff00")
+CYAN        = rgb("00ffff")
+MAGENTA     = rgb("ff00ff")
+
+WHITE       = rgb("ffffff")
+
+ORANGE      = rgb("ff7f00")
+LIME        = rgb("aeff00")
+TURQUOISE   = rgb("00ffb3")
+SKY         = rgb("0099ff")
+PURPLE      = rgb("9900ff")
